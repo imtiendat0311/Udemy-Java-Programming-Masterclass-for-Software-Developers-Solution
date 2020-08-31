87c5706a0ad5ebb08f209764381d787ec3daa475
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2020-08-31 15:32:47 +0700

    Update MegaBytesConverter.java

--- Section 4/MegaBytesConverter.java
+++ Section 4/MegaBytesConverter.java
@@ -1,17 +1,12 @@
 public class MegaBytesConverter {
-    public static void printMegaBytesAndKiloBytes(int kiloBytes){
-    if(kiloBytes<0){
-        System.out.println("Invalid Value");
-    }
-    int megaBytes = kiloBytes/1024;
-    int remainKiloBytes = kiloBytes%1024;
-    System.out.println(kiloBytes+" KB = "+megaBytes+" MB and "+remainKiloBytes+" KB");
-    }
-    public static void main (String args[]){
-        printMegaBytesAndKiloBytes(2500); 
-
-        printMegaBytesAndKiloBytes(-1024); 
-
-        printMegaBytesAndKiloBytes(5000);
-    }
+       public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes<0){
+            System.out.println("Invalid Value");
+        }
+        else{
+            int megaBytes = kiloBytes/1024;
+            int remainKiloBytes = kiloBytes%1024;
+            System.out.println(kiloBytes+" KB = "+megaBytes+" MB and "+remainKiloBytes+" KB");
+        }
+      }
 }
