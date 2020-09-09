e5f99b9c7e53e30be51126f51654408d124a799c
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2020-09-09 16:50:37 +0700

    Add files via upload

---
+++ Section 6/Cuboid.java
@@ -0,0 +1,14 @@
+public class Cuboid extends Rectangle{
+    private double height;
+    public Cuboid (double height,double width,double length){
+        super(width,length);
+        if(height<0){height=0;}
+        this.height=height;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public double getVolume(){
+        return height*getArea();
+    }
+}
